bffeaf790bb32567033d273193304cecb4fbeefd
Ole Algoritme <olealgoritme@gmail.com>
2019-08-28 17:09:01 +0200

    update

---
+++ src/test/java/YatzyTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class YatzyTest {
+
+    @Test
+    void yatzyTest() {
+        assertEquals(4, 2+2);
+    }
+
+    @Test
+    void shouldCalculateForOnes() {
+    }
+}
