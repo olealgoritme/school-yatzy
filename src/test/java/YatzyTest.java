dd0d0cf355a7bcecbe792d1fcff58e239d4fe223
Ole Algoritme <olealgoritme@gmail.com>
2019-09-09 13:00:11 +0200

    pairs

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class YatzyTest {
@@ -19,21 +22,37 @@ class YatzyTest {
         assertEquals(5, score("FIVES", new int[] {2, 5, 1, 1, 1}));
     }
 
-
     @Test
     void shouldCalculateForPairs(){
-        assertEquals(10, score("PAIRS", new int[]{2, 5, 5, 2, 1}));
+        assertArrayEquals(new int[] {2, 5}, pairsOf(new int[]{2, 5, 5, 2, 1}));
     }
 
-   /*private boolean frequency(int expected, int[]dice) {
-        int result = 0;
-        for (int diceValue : dice) {
-            if (diceValue == dice[diceValue])
-                result++;
+    int[] pairsOf(int[] dice) {
+        // returns array of digits that have pairs (2 equal values)
+        ArrayList<Integer> pairs = new ArrayList<Integer>();
+
+        for(int y = 0; y < dice.length; y++) {
+            int current = dice[y];
+            int pos = y;
+
+            for(int i = pos+1; i < dice.length; i++) {
+                if(dice[i] == current) {
+                    // we have a fucking match
+                    pairs.add(current);
+                }
+            }
         }
-        return (expected == result);
-    }*/
-   private int score(String category, int[] dice) {
+        int pos = 0;
+        int[] p = new int[pairs.size()];
+        for (int i : pairs) {
+            p[pos] = i;
+            pos++;
+        }
+        return p;
+    }
+
+
+    private int score(String category, int[] dice) {
 
        if(category.equals("ONES")) {
            return frequency(1, (dice));
